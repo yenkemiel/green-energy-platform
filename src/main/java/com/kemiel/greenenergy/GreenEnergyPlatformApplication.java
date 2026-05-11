7f7323f2998141784c7c86f6bb261ae9eec6310f
Kemiel Yen <102275234+yenkemiel@users.noreply.github.com>
2026-05-11 21:12:00 +0800

    chore: initial project setup

---
+++ src/main/java/com/kemiel/greenenergy/GreenEnergyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.kemiel.greenenergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenEnergyPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenEnergyPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kemiel/greenenergy/GreenEnergyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kemiel.greenenergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenEnergyPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
