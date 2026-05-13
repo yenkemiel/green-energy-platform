dfc26530ff16242c75fd293cfa15322c228323ce
Kemiel Yen <102275234+yenkemiel@users.noreply.github.com>
2026-05-13 13:32:26 +0800

    chore: add JacksonConfig for LocalDateTime serialization

---
+++ src/main/java/com/kemiel/greenenergy/config/JacksonConfig.java
@@ -0,0 +1,15 @@
+package com.kemiel.greenenergy.config;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer customizer(){
+        return builder -> builder.modules(new JavaTimeModule());
+    }
+}
