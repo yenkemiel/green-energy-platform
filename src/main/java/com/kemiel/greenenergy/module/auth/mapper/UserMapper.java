a363c100d36401e826902d5f79d4c97e61c64258
Kemiel Yen <102275234+yenkemiel@users.noreply.github.com>
2026-05-19 14:39:38 +0800

    feat: add selectList and updateStatusById to UserMapper

--- src/main/java/com/kemiel/greenenergy/module/auth/mapper/UserMapper.java
+++ src/main/java/com/kemiel/greenenergy/module/auth/mapper/UserMapper.java
@@ -1,7 +1,11 @@
 package com.kemiel.greenenergy.module.auth.mapper;
 
+import com.kemiel.greenenergy.common.enums.RoleType;
 import com.kemiel.greenenergy.module.auth.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 
 /**
@@ -11,6 +15,8 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserMapper {
     User selectById(Long id);
     User selectByUsername(String username);
+    List<User> selectList(@Param("role")RoleType role);
     void insert(User user);
     void updateById(User user);
+    void updateStatusById(@Param("id") Long id, @Param("isActive") Boolean isActive);
 }
