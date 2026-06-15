d19cdba8b789eb9b70893f00b03c83ca30678bc4
Kemiel Yen <102275234+yenkemiel@users.noreply.github.com>
2026-06-15 22:02:26 +0800

    fix: enable CORS in SecurityConfig

--- src/main/java/com/kemiel/greenenergy/config/SecurityConfig.java
+++ src/main/java/com/kemiel/greenenergy/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configure(http))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
