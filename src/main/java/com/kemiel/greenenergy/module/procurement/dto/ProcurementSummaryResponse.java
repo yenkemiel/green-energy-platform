005dc90d27c846e69bd79ea259a07fcab25bb10b
Kemiel Yen <102275234+yenkemiel@users.noreply.github.com>
2026-06-26 14:50:55 +0800

    feat: add ProcurementSummaryResponse DTO

---
+++ src/main/java/com/kemiel/greenenergy/module/procurement/dto/ProcurementSummaryResponse.java
@@ -0,0 +1,17 @@
+package com.kemiel.greenenergy.module.procurement.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * 採購憑證庫存總覽回傳格式。
+ */
+@Getter
+@Builder
+public class ProcurementSummaryResponse {
+
+    private Integer totalQuantity;
+    private Integer usedQuantity;
+    private Integer availableQuantity;
+    private Integer expiringWithin30Days;
+}
