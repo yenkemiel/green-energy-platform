0346eaaab278415d98653af16bce42f7d97b0cf8
Kemiel Yen <102275234+yenkemiel@users.noreply.github.com>
2026-06-30 20:27:37 +0800

    feat: enable scheduling in GreenEnergyPlatformApplication

--- src/main/java/com/kemiel/greenenergy/GreenEnergyPlatformApplication.java
+++ src/main/java/com/kemiel/greenenergy/GreenEnergyPlatformApplication.java
@@ -2,12 +2,13 @@ package com.kemiel.greenenergy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GreenEnergyPlatformApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GreenEnergyPlatformApplication.class, args);
     }
-
 }
+
